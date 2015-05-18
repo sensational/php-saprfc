7822f4d460bd1dbd258b7f64480a1f26b959635f
Philip Hofstetter <phofstetter@sensational.ch>
2015-05-18 12:51:03 +0200

    fix compilation on PHP 5.4 and later
    still throwing warnings, but this compiles (and runs fine on our
    infrastructure)

--- saprfc.c
+++ saprfc.c
@@ -44,7 +44,7 @@ static int le_rfc, le_function;
 
 /* Every user visible function must have an entry in saprfc_functions[].
 */
-function_entry saprfc_functions[] = {
+zend_function_entry saprfc_functions[] = {
     PHP_FE(saprfc_open,    NULL)
     PHP_FE(saprfc_function_discover,    NULL)
     PHP_FE(saprfc_function_define,    NULL)
